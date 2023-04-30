65bcb1bcb832f753627233cbd86fbbb1f6e4862d
Tux <82789164+fukboi69420@users.noreply.github.com>
2023-04-29 21:56:35 -0700

    Update video_downloader.sh

--- scripts/other/video_downloader.sh
+++ scripts/other/video_downloader.sh
@@ -1,11 +1,29 @@
-# update
-sudo apt update
+#!/bin/bash
 
-# uinstall snapd
-sudo apt install snapd
+function install_video_downloader() {
+    # This function installs the video-downloader snap package from snapcraft.io
 
-# install core
-sudo snap install core
+    # Install snapd if not already installed
+    if ! command -v snap &> /dev/null
+    then
+        echo "snapd not found. Installing snapd..."
+        sudo apt update
+        sudo apt install snapd -y
+    fi
 
-# install video-downloader
-sudo snap install video-downloader
+    # Install video-downloader snap package
+    echo "Installing video-downloader snap package..."
+    sudo snap install video-downloader
+
+    # Check if installation was successful
+    if ! command -v video-downloader &> /dev/null
+    then
+        echo "Installation failed. Please try again."
+        exit 1
+    fi
+
+    echo "Installation successful. You can now use the video-downloader command."
+}
+
+# Call the function to install video-downloader
+install_video_downloader
